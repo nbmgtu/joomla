1975defb36b312bca97eaf978301081fa61e724a
starodub <starodub.xxx@gmail.com>
2017-08-09 22:12:31 +0300

    ...

--- .databases/all.sql
+++ .databases/all.sql
@@ -3,4 +3,8 @@
 \. ./joomla.import_content.SQL
 \. ./joomla.import_banner.SQL
 \. ./joomla.import_menu.SQL
+\. ./joomla.import_modules.SQL
 \. ./joomla.import.SQL
+
+
+-- Extensions / Calendars & Events / Events / JEvents
